29653eda22f1ed7864384647d9f9c5e2bf3330fa
nikneym <halildrk@gmail.com>
2023-02-08 22:36:29 +0300

    Frontend: Introduce header area

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,8 @@ export default function App() {
   return (
     <div className="App">
       <Header />
+      <div className="wave bg-zinc-900"></div>
+      body here
     </div>
   )
 }
--- frontend/src/Header.jsx
+++ frontend/src/Header.jsx
@@ -1,9 +1,18 @@
 export default function Header() {
   return (
-    <>
-    <span className="">
-      Ucuz Kitap
-    </span>
-    </>
+    <div className="bg-zinc-900 mx-auto text-center">
+      <div className="relative inline-block mb-5 pt-12">
+        <h1 className="relative block text-6xl font-extrabold text-purple-400 font-zilla drop-shadow-lg">
+          Ucuz Kitapp
+        </h1>
+        <h4 className="relative block text-left bottom-2 font-zilla text-amber-200 drop-shadow-sm">
+          💰 Ölücülerin adresi.
+        </h4>
+      </div>
+
+      <div className="mx-auto block">
+        <input className="inline-block w-6/12 border-4 border-purple-400 rounded-full text-left py-4 px-6 text-xl bg-zinc-800 shadow-inner outline-none" type="text" placeholder="1984, Dijital Kale, Kayıp Tanrılar Ülkesi..." />
+      </div>
+    </div>
   )
 }
